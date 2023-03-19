dbd5bd97c5f1b768543a22502d1233ebf2a0647e
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-03-19 18:40:15 +0200

    add brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import hello from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+hello();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
