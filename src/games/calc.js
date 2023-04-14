f4781b7fcf9b7f6e3fab8500fd3bf8c7893aba80
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-04-14 11:20:49 +0300

    add function in calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -7,8 +7,19 @@ export default () => {
     const simbols = ['+', '-', '*'];
     const randomIndex = Math.floor(Math.random() * (simbols.length - 1));
     const randomSimbol = simbols[randomIndex];
+    let answer = 0;
+      switch (randomIndex) {
+        case 0:
+          answer = number + number;
+          break;
+        case 1:
+          answer = number - number;
+          break;
+        case 2:
+          answer = number * number;
+          break;
+    }
     const question = `${number} ${randomSimbol} ${number}`;
-    const answer = eval(question);
     return [question, answer.toString()];
   };
   gameLogic(rules, rulesFunction);
